1aad9524f1415c4c73f12a971cfe161c7480b4a9
Tcharlles <tcharlles3@gmail.com>
2023-05-23 11:52:02 -0300

    implement-git

---
+++ faq.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("quest-faq-depen");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active-faq");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
